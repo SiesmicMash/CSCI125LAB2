9b8cd866e8d304e1fd39f9db687298fe09ab6414
SiesimcMash <usaidsyedali@gmail.com>
2024-09-23 22:15:26 -0700

    First part of the lab

--- CSCI125LAB2.cpp
+++ CSCI125LAB2.cpp
@@ -1,20 +1,83 @@
-// CSCI125LAB2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+* This is a program that takes in the weight of a person and calculates the basal metabolic rate of a user gives thier weight in pounds
+* 
+* Account for the calories burned through physical activity
+* 
+*/
 
 #include <iostream>
+#include <cmath>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+double basalMetabolicRate(int weight);
+double caloriesForPhysExercise(int minutes, int intensity, int weight);
+void input();
+int validUserWeight(int weight);
+int validMinutes(int minutes);
+int validIntensity(int intensity);
+
+
+int main() {
+	input();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int validUserWeight(int weight) {
+	if (weight >= 0) {
+		return weight;
+	}
+	else {
+		std::cout << "PLease input a valid weight in pounds: "<< std::endl;
+	}
+}
+int validMinutes(int minutes) {
+	if (minutes >= 0) {
+		return minutes;
+	}
+	else {
+		std::cout << "Please input a valid number of minutes: " << std::endl;
+	}
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int validIntensity(int intensity) {
+	if ((intensity == 17 || intensity == 10) || (intensity == 8 || intensity == 1)) {
+		return intensity;
+	}
+	else {
+		std::cout << "Please input a valid intensity (17, 10, 8, or 1): "<<std::endl;
+	}
+}
+
+double basalMetabolicRate(int weight) {
+	return (70 * pow((weight / 2.2), 0.756)); 
+}
+
+double caloriesForPhysExercise(int minutes, int intensity, int weight) {
+	return ((0.0385 * intensity) * (weight * minutes)); 
+}
+
+void input() {
+	bool loop = true;
+	int userInput, userWeight, minutes, intensity;
+
+
+	while (loop) {
+		std::cout << "Welcome to the calorie per serving calculator! "<<std::endl
+			<< " 1. To calculate the required calories per serving, we will first need to calculate the basal metabolic rate" <<std::endl
+			<< " 2. We will also need to calculate the amount of calories burned during a physical activity" <<std::endl
+			<< " 3. We will combine the 2 to produce the total calories per serving"<<std::endl
+			<< " 4. Exit" <<std::endl;
+
+		switch (userInput) {
+		case 1:
+			std::cout << "Please enter your weight in pounds: " << std::endl;
+			std::cin >> userWeight;
+			int validWeight = validUserWeight(userWeight);
+			double basalMR = basalMetabolicRate(validWeight);
+			std::cout << "The calories used by your body for the basal metabolic rate is, " << basalMR << std::endl;
+		case 2:
+			std:
+		}
+
+	}
+}
